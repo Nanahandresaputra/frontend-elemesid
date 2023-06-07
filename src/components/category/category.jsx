7350b696dd8a86414fcdf51ec8b7d5198008cbf7
Nanahandresaputra <nanaggmu@gmail.com>
2023-06-07 23:04:00 +0700

    second commit

--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -10,6 +10,39 @@ import doughnutVector from "../../assets/doughnutVector.png";
 import salmonVector from "../../assets/salmonVector.png";
 
 const Category = () => {
+  const dummyData = [
+    {
+      name: "Cupcake",
+      items: "22 items",
+      img: cupcakeVector,
+      bgcolor: "#F0FEEB",
+    },
+    {
+      name: "Pizza",
+      items: "25 items",
+      img: pizzaVector,
+      bgcolor: "#E6F3F5",
+    },
+    {
+      name: "Kebab",
+      items: "12 items",
+      img: kebabVector,
+      bgcolor: "#EAEEFA",
+    },
+    {
+      name: "Salmon",
+      items: "22 items",
+      img: salmonVector,
+      bgcolor: "#F9EEF3",
+    },
+    {
+      name: "Doghnut",
+      items: "22 items",
+      img: doughnutVector,
+      bgcolor: "#F3F7D9",
+    },
+  ];
+
   return (
     <div className="mt-5">
       <Container>
@@ -22,22 +55,16 @@ const Category = () => {
             slidesPerView: 2,
             spaceBetween: 200,
           },
-          // when window width is >= 480px
-          480: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-          },
-          // when window width is >= 640px
           640: {
-            slidesPerView: 4,
-            spaceBetween: 40,
+            slidesPerView: 3,
+            spaceBetween: 150,
           },
           1020: {
             slidesPerView: 4,
-            spaceBetween: 100,
+            spaceBetween: 30,
           },
         }}
-        //   centeredSlides={true}
+        centeredSlides={true}
         slidesPerView={4}
         spaceBetween={100}
         navigation={true}
@@ -47,66 +74,20 @@ const Category = () => {
         modules={[Pagination]}
         className="mySwiper mt-5 w-75"
       >
-        <SwiperSlide className="mb-5">
-          <Card style={{ width: "15rem", backgroundColor: "#F0FEEB" }}>
-            <Card.Body>
-              <div className="d-flex flex-column justify-content-center align-items-center">
-                <Image src={cupcakeVector} height={50} width={50} />
-                <p className="text-center">
-                  <span className="fw-bold fs-5">Cupcake</span> <br /> 22 items
-                </p>
-              </div>
-            </Card.Body>
-          </Card>
-        </SwiperSlide>
-        <SwiperSlide className="mb-5">
-          <Card style={{ width: "15rem", backgroundColor: "#E6F3F5" }}>
-            <Card.Body>
-              <div className="d-flex flex-column justify-content-center align-items-center">
-                <Image src={pizzaVector} height={50} width={50} />
-                <p className="text-center">
-                  <span className="fw-bold fs-5">Pizza</span> <br /> 25 items
-                </p>
-              </div>
-            </Card.Body>
-          </Card>
-        </SwiperSlide>
-        <SwiperSlide className="mb-5">
-          <Card style={{ width: "15rem", backgroundColor: "#EAEEFA" }}>
-            <Card.Body>
-              <div className="d-flex flex-column justify-content-center align-items-center">
-                <Image src={kebabVector} height={50} width={50} />
-                <p className="text-center">
-                  <span className="fw-bold fs-5">Kebab</span> <br /> 12 items
-                </p>
-              </div>
-            </Card.Body>
-          </Card>
-        </SwiperSlide>
-        <SwiperSlide className="mb-5">
-          <Card style={{ width: "15rem", backgroundColor: "#F9EEF3" }}>
-            <Card.Body>
-              <div className="d-flex flex-column justify-content-center align-items-center">
-                <Image src={salmonVector} height={50} width={50} />
-                <p className="text-center">
-                  <span className="fw-bold fs-5">Salmon</span> <br /> 22 items
-                </p>
-              </div>
-            </Card.Body>
-          </Card>
-        </SwiperSlide>
-        <SwiperSlide className="mb-5">
-          <Card style={{ width: "15rem", backgroundColor: "#F3F7D9" }}>
-            <Card.Body>
-              <div className="d-flex flex-column justify-content-center align-items-center">
-                <Image src={doughnutVector} height={50} width={50} />
-                <p className="text-center">
-                  <span className="fw-bold fs-5">Doughnut</span> <br /> 22 items
-                </p>
-              </div>
-            </Card.Body>
-          </Card>
-        </SwiperSlide>
+        {dummyData?.map((index, i) => (
+          <SwiperSlide className="mb-5" key={i}>
+            <Card style={{ width: "15rem", backgroundColor: index.bgcolor }}>
+              <Card.Body>
+                <div className="d-flex flex-column justify-content-center align-items-center">
+                  <Image src={index.img} height={50} width={50} />
+                  <p className="text-center">
+                    <span className="fw-bold fs-5">{index.name}</span> <br /> {index.items}
+                  </p>
+                </div>
+              </Card.Body>
+            </Card>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
